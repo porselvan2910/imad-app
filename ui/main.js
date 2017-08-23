6d71e576db3b5f3666f3e233a52f7c8cbdcfad8b
porselvan2910 <30888768+porselvan2910@users.noreply.github.com>
2017-08-23 14:37:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,8 @@ console.log('Javascript is awesome');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
+//Move the image
+var img = document.getElementById('img');
+img.onclick = function (){
+    img.style.marginLeft = '100px';
+};
