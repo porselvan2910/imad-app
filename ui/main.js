1edab157ecaeac1a2e5d6d8d6e13f1fa2d64a84f
porselvan2910 <30888768+porselvan2910@users.noreply.github.com>
2017-08-23 14:47:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    image.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
-    img.style.marginLeft = '100px';
+    var interval = setinterval(moveRight, 100);
 };
